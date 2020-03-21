0279321037ff263442d7ee9b9b5fa66db1249491
notjustmoney <chajy97@gmail.com>
2020-03-21 22:04:27 +0900

    modify 15354.cpp

--- notjustmoney/20200320/15354.cpp
+++ notjustmoney/20200320/15354.cpp
@@ -1,34 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-char S[50001];
-int A,B,C,D;
-bool isPossible() {
-	int a1[26] = {0};
-	int a2[26] = {0};
-	int i;
-	for(i=A; i<=B; i++) {
-		a1[S[i]-'a']++;
-	}
-	for(i=C; i<=D; i++) {
-		a2[S[i]-'a']++;
-	}
-	for(i=0; i<26; i++) {
-		if(a2[i] < a1[i]) return false;
-	}
-	return true;
-}
-
 int main() {
-	scanf("%s", S+1);
+	int N;
+	int count = 2;
+	scanf("%d", &N);
 	
-	int Q;
-	scanf("%d", &Q);
-	for(int i=0; i<Q; i++) {
-		scanf("%d %d %d %d", &A, &B, &C, &D);
-		if(isPossible()) printf("DA\n");
-		else printf("NE\n");
+	char C1, C2;
+	for(int i = 0; i < N; i++) {
+		getchar();
+		scanf("%c", &C1);
+		if(i == 0) C2 = C1;
+		if(C1 != C2){
+			count++;
+			C2 = C1;	
+		}
 	}
+	printf("%d\n", count);
 	
-	return 0;
+	return 0;	
 }
