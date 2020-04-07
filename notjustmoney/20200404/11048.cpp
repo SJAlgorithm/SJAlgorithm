985e1b49557330e1746c17b18919fe279ae922ac
notjustmoney <chajy97@gmail.com>
2020-04-07 21:58:47 +0900

    20200404 solution code

---
+++ notjustmoney/20200404/11048.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int miro[1001][1001];
+int dp[1001][1001];
+int n, m;
+
+int main() {
+	scanf("%d%d", &n, &m);
+	for(int i = 1; i <= n; i++) 
+	for(int j = 1; j <= m; j++) 
+		scanf("%d", &miro[i][j]);
+		
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= m; j++) {
+			dp[i][j] = max(dp[i-1][j-1], max(dp[i-1][j], dp[i][j-1]));
+			dp[i][j] += miro[i][j];
+		}
+	}
+	printf("%d\n", dp[n][m]);
+	
+	return 0;	
+}
