daa328036be1d9c4b51d7556b39d27a826a78385
9mean <kanggm102@naver.com>
2022-03-20 22:15:40 +0900

    RETRY

--- 9mean/20220320/19539.py
+++ 9mean/20220320/19539.py
@@ -1,37 +1,14 @@
-s = input()
-table = {
-    'A': 3,
-    'B': 2,
-    'C': 1,
-    'D': 2,
-    'E': 3,
-    'F': 3,
-    'G': 3,
-    'H': 3,
-    'I': 1,
-    'J': 1,
-    'K': 3,
-    'L': 1,
-    'M': 3,
-    'N': 3,
-    'O': 1,
-    'P': 2,
-    'Q': 2,
-    'R': 2,
-    'S': 1,
-    'T': 2,
-    'U': 1,
-    'V': 1,
-    'W': 2,
-    'X': 2,
-    'Y': 2,
-    'Z': 1
-}
-res = 0
-for x in s:
-    res += table[x]
-    res = res % 10
-if res % 2 == 0:
-    print("You're the winner?")
+n = int(input())
+tree = list(map(int, input().split()))
+flag = 0
+sum2 = 0
+sum1 = sum(tree)
+for x in tree:
+    sum2 += x // 2
+if sum1 % 3 == 0:
+    if sum2 >= sum1 // 3:
+        print("YES")
+    else:
+        print("NO")
 else:
-    print("I'm a winner!")
+    print("NO")
